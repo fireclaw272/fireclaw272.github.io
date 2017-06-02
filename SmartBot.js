f4308a2f2b2479e18a9cedd4a03eb0112e7d44dd
fireclaw272 <firedragon272@gmail.com>
2017-06-01 23:39:13 -0700

    uploading smartbot

--- SmartBot.js
+++ SmartBot.js
@@ -19,8 +19,6 @@ var main = function(){
 	$('<a>').appendTo('.link6').attr("href","http://cottonarmadillo.freetzi.com/recipe/").text('Recipe');
 	$('<li>').appendTo('.menu ul').addClass('link7');
 	$('<a>').appendTo('.link7').attr("href","http://cottonarmadillo.freetzi.com/cashier/").text('Cashier');
-	$('<li>').appendTo('.menu ul').addClass('link8');
-	$('<a>').appendTo('.link8').attr("href","http://cottonarmadillo.freetzi.com/").text('Timer');
 	//$('<li>').appendTo('.menu ul').addClass('link____');
 	//$('<a>').appendTo('.link____').attr("href","http://_______________").text('____');
 	
@@ -41,6 +39,35 @@ var main = function(){
 			left: "0px"
 		}, 200);
 	});
-};
+	
+	$('.button').click(function(){
+		var questions = $('.thought-box').val();
+		var current = $('.current');
+		var next = current.next();
+		$('<ul>').appendTo('.list').addClass('current').addClass('question');
+		$('<li>').text(questions).appendTo('.current').addClass('question-list-format');
+		$('ul').removeClass('current');
+		$('<ul>').appendTo('.list').addClass('reply').addClass('current');
+		$('<li>').text('Shut up!').addClass('reply-list-format').appendTo('.current');
+		$('ul').removeClass('current');
+		$('.thought-box').val("");
+	});
+	
+	
+	$('.thought-box').keyup(function(event){
+		if(event.which===13) {
+			var questions = $('.thought-box').val();
+			var current = $('.current');
+			var next = current.next();
+			$('<ul>').appendTo('.list').addClass('current').addClass('question');
+			$('<li>').text(questions).appendTo('.current').addClass('question-list-format');
+			$('ul').removeClass('current');
+			$('<ul>').appendTo('.list').addClass('reply').addClass('current');
+			$('<li>').text('Shut up!').addClass('reply-list-format').appendTo('.current');
+			$('ul').removeClass('current');
+			$('.thought-box').val("");
+		};
+	});
 
+};
 $(document).ready(main);
