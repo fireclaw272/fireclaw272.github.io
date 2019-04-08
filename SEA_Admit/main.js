492c3b59230c6b5f239c906e827891755d6e62db
fireclaw272 <firedragon272@gmail.com>
2019-04-08 11:35:05 -0700

    Add files via upload

---
+++ SEA_Admit/main.js
@@ -0,0 +1,26 @@
+window.addEventListener('load', main);
+function main(){
+    var button = document.getElementById("button");
+    var textBox = document.getElementById("answerBox");
+    var targetURL = "https://google.com";
+    
+    function checkAnswer(){
+        var value = answerBox.value;
+        //Hello, if you are ooking to the answer to the question, it is right here. I was to lazy to make the answer invisible so have fun, but the letter I wrote for my sister is just a joke letter, so you probably don't even want to read it.
+        if(value == "johnny" || value == "Johnny"){
+           window.location = targetURL;
+        }
+        else{
+            answerBox.value = "";
+            var paragraph = document.createElement("p");
+            paragraph.classList.add('error');
+            var node = document.createTextNode("Try Again");
+            paragraph.appendChild(node);
+            
+            var div = document.getElementById("container");
+            div.insertBefore(paragraph,textBox);
+        }
+    }
+    
+    button.addEventListener("click", checkAnswer);
+}
